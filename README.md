68e11086df66c6de25b54642101753bfbe34bfac
Kracekumar <me@kracekumar.com>
2021-09-06 00:21:08 +0530

    First commit with data files

--- README.md
+++ README.md
@@ -1,2 +1,141 @@
 # pulse-plus
 Utilities to explore, convert PhonePe [Pulse data](https://github.com/PhonePe/pulse)
+
+# Installation
+
+1. Checkout the repo.
+2. Install dependencies. `poetry install`. Use Python 3.9
+3. Now the installation should have all the dependencies.
+4. Check out the pulse data repo - https://github.com/PhonePe/pulse
+
+# Usage
+
+1. Convert the data directory into sqlite db.
+
+   ```bash
+   $poetry run python pulse/cli.py ../pulse/data --output pulse.db
+   aggregated_user table created
+   aggregated_user_device table created
+   aggregated_transaction table created
+   top_user table created
+   top_transaction table created
+   Inserted 5698 records in aggregated_user_device
+   Inserted 518 records in aggregated_user
+   Inserted 2584 records in aggregated_transaction
+   Inserted 9562 records in top_user
+   Inserted 9561 records in top_transaction
+   pulse.db created
+   ```
+
+2. Check the created database
+
+   ```bash
+   $poetry run sqlite-utils pulse.db 'select count(*) from aggregated_user' -t
+     count(*)
+   ----------
+          518
+   $poetry run sqlite-utils pulse.db 'select * from aggregated_user limit 1' -t
+     id  aggregated_by    aggregate_name               year    users    app_opens  start_date    end_date
+   ----  ---------------  -------------------------  ------  -------  -----------  ------------  ----------
+      1  state            andaman-&-nicobar-islands    2019    18596            0  2019-01-01    2019-03-31
+   
+   ```
+
+   
+
+   
+
+   ## All tables schema
+
+   
+
+   ```sql
+   CREATE TABLE [aggregated_user] (
+      [id] INTEGER PRIMARY KEY NOT NULL,
+      [aggregated_by] TEXT NOT NULL,
+      [aggregate_name] TEXT NOT NULL,
+      [year] INTEGER NOT NULL,
+      [users] INTEGER NOT NULL,
+      [app_opens] INTEGER NOT NULL,
+      [start_date] TEXT NOT NULL,
+      [end_date] TEXT NOT NULL
+   );
+   CREATE TABLE [aggregated_user_device] (
+      [id] INTEGER PRIMARY KEY NOT NULL,
+      [aggregated_by] TEXT NOT NULL,
+      [aggregate_name] TEXT NOT NULL,
+      [year] INTEGER NOT NULL,
+      [start_date] TEXT NOT NULL,
+      [end_date] TEXT NOT NULL,
+      [brand] TEXT NOT NULL,
+      [count] INTEGER NOT NULL,
+      [percentage] FLOAT NOT NULL
+   );
+   CREATE TABLE [aggregated_transaction] (
+      [id] INTEGER PRIMARY KEY NOT NULL,
+      [aggregated_by] TEXT NOT NULL,
+      [aggregate_name] TEXT NOT NULL,
+      [year] INTEGER NOT NULL,
+      [start_date] TEXT NOT NULL,
+      [end_date] TEXT NOT NULL,
+      [name] TEXT NOT NULL,
+      [type] TEXT NOT NULL,
+      [count] INTEGER NOT NULL,
+      [amount] FLOAT NOT NULL
+   );
+   CREATE TABLE [top_user] (
+      [id] INTEGER PRIMARY KEY NOT NULL,
+      [aggregated_by] TEXT NOT NULL,
+      [aggregate_name] TEXT NOT NULL,
+      [year] INTEGER NOT NULL,
+      [start_date] TEXT NOT NULL,
+      [end_date] TEXT NOT NULL,
+      [name] TEXT NOT NULL,
+      [type] TEXT NOT NULL,
+      [registered_users] INTEGER NOT NULL
+   );
+   CREATE TABLE [top_transaction] (
+      [id] INTEGER PRIMARY KEY,
+      [aggregated_by] TEXT,
+      [aggregate_name] TEXT,
+      [year] INTEGER,
+      [start_date] TEXT,
+      [end_date] TEXT,
+      [entity_name] TEXT,
+      [entity_type] TEXT,
+      [type] TEXT,
+      [count] INTEGER,
+      [amount] FLOAT
+   );
+   
+   ```
+
+# Functionalities
+
+- Convert the pulse repo into single sqlite db.
+- Converted sqlite [database](data/v1/pulse.db) and five csv files: [data/v1/aggregated_user_device.csv](data/v1/aggregated_user_device.csv), [data/v1/aggregated_user.csv](data/v1/aggregated_user.csv), [data/v1/aggregated_transaction.csv](data/v1/aggregated_transaction.csv), [data/v1/top_transaction.csv](data/v1/top_transaction.csv), [data/v1/top_user.csv](data/v1/top_user.csv).
+
+# CSV sample 
+
+```bash
+$head -4 data/v1/top_user.csv
+id,aggregated_by,aggregate_name,year,start_date,end_date,name,type,registered_users
+1,state,andaman-&-nicobar-islands,2019,2019-01-01,2019-03-31,744103,pincodes,4136
+2,state,andaman-&-nicobar-islands,2019,2019-01-01,2019-03-31,744101,pincodes,3030
+3,state,andaman-&-nicobar-islands,2019,2019-01-01,2019-03-31,744105,pincodes,2994
+
+$head -4 data/v1/top_transaction.csv
+id,aggregated_by,aggregate_name,year,start_date,end_date,entity_name,entity_type,type,count,amount
+1,state,andaman-&-nicobar-islands,2019,2019-01-01,2019-03-31,744103,pincodes,TOTAL,5742,19401964.77965755
+2,state,andaman-&-nicobar-islands,2019,2019-01-01,2019-03-31,744101,pincodes,TOTAL,5156,15501306.227195919
+3,state,andaman-&-nicobar-islands,2019,2019-01-01,2019-03-31,744102,pincodes,TOTAL,3925,16683547.094795756
+
+$head -4 data/v1/aggregated_transaction.csv
+id,aggregated_by,aggregate_name,year,start_date,end_date,name,type,count,amount
+1,state,andaman-&-nicobar-islands,2019,2019-01-01,2019-03-31,Recharge & bill payments,TOTAL,15263,6611459.8729725825
+2,state,andaman-&-nicobar-islands,2019,2019-01-01,2019-03-31,Peer-to-peer payments,TOTAL,13119,91808345.86839552
+3,state,andaman-&-nicobar-islands,2019,2019-01-01,2019-03-31,Merchant payments,TOTAL,1759,3266589.8469614363
+```
+
+
+
---
+++ pulse/cli.py
@@ -0,0 +1,411 @@
+import click
+import rich
+from rich.console import Console
+from pathlib import Path
+from sqlite_utils import Database
+import sqlite3
+import datetime
+from dateutil.relativedelta import relativedelta
+import json
+
+
+error_console = Console(stderr=True, style="bold red")
+info_console = Console(style="black green")
+print = info_console.print
+error_print = error_console.print
+
+
+TABLE_NAMES = {
+    "aggregated_transaction": "aggregated_transaction",
+    "aggregated_user": "aggregated_user",
+    "aggregated_user_device": "aggregated_user_device",
+    "top_transaction": "top_transaction",
+    "top_user": "top_user",
+}
+
+
+# Tables
+
+
+def create_tables(db):
+    # TODO: Create common base dict fields
+    aggregated_user_fields = {
+        "id": int,
+        "aggregated_by": str,
+        "aggregate_name": str,
+        "year": int,
+        "users": int,
+        "app_opens": int,
+        "start_date": datetime.date,
+        "end_date": datetime.date,
+    }
+    aggregated_user_device_fields = {
+        "id": int,
+        "aggregated_by": str,
+        "aggregate_name": str,
+        "year": int,
+        "start_date": datetime.date,
+        "end_date": datetime.date,
+        "brand": str,
+        "count": int,
+        "percentage": float,
+    }
+    aggregated_transaction_fields = {
+        "id": int,
+        "aggregated_by": str,
+        "aggregate_name": str,
+        "year": int,
+        "start_date": datetime.date,
+        "end_date": datetime.date,
+        "name": str,
+        "type": str,
+        "count": int,
+        "amount": float,
+    }
+    top_user_fields = {
+        "id": int,
+        "aggregated_by": str,
+        "aggregate_name": str,
+        "year": int,
+        "start_date": datetime.date,
+        "end_date": datetime.date,
+        "name": str,
+        "type": str,
+        "registered_users": int,
+    }
+    top_transaction_fields = {
+        "id": int,
+        "aggregated_by": str,
+        "aggregate_name": str,
+        "year": int,
+        "start_date": datetime.date,
+        "end_date": datetime.date,
+        "entity_name": str,
+        "entity_type": str,
+        "type": str,
+        "count": int,
+        "amount": float,
+    }
+    for name, fields in (
+        ("aggregated_user", aggregated_user_fields),
+        ("aggregated_user_device", aggregated_user_device_fields),
+        ("aggregated_transaction", aggregated_transaction_fields),
+        ("top_user", top_user_fields),
+        ("top_transaction", top_transaction_fields),
+    ):
+        table = TABLE_NAMES[name]
+        try:
+            if name == "top_transaction":
+                db[table].create(
+                    fields, pk="id", not_null=set(fields.keys()).remove("entity_name")
+                )
+            else:
+                db[table].create(fields, pk="id", not_null=set(fields.keys()))
+            print(f"{table} table created")
+        except sqlite3.OperationalError:
+            error_print(f"{table} already exists. Skipping table creation")
+
+
+def get_db(name):
+    return Database(name)
+
+
+def is_state(name):
+    return "state" in str(name)
+
+
+def get_date_range(name, year):
+    """Get date range for the file."""
+    if name == "1.json":
+        start_date = datetime.date(year=year, month=1, day=1)
+    elif name == "2.json":
+        start_date = datetime.date(year=year, month=4, day=1)
+    elif name == "3.json":
+        start_date = datetime.date(year=year, month=7, day=1)
+    elif name == "4.json":
+        start_date = datetime.date(year=year, month=10, day=1)
+    return {
+        "start_date": start_date,
+        "end_date": start_date + relativedelta(months=+3, days=-1),
+    }
+
+
+def get_aggregated_user_from_file(path):
+    data = json.load(open(path))["data"]
+    return {
+        "users": data["aggregated"]["registeredUsers"],
+        "app_opens": data["aggregated"]["appOpens"],
+    }
+
+
+def get_aggregated_user_device_from_file(path):
+    data = json.load(open(path))["data"]["usersByDevice"]
+    return [
+        {
+            "brand": datum["brand"],
+            "count": datum["count"],
+            "percentage": datum["percentage"],
+        }
+        for datum in data
+    ]
+
+
+def get_aggregated_transaction_from_file(path):
+    data = json.load(open(path))["data"]
+    data = data["transactionData"]
+    return [
+        {
+            "name": datum["name"],
+            "type": datum["paymentInstruments"][0]["type"],
+            "count": datum["paymentInstruments"][0]["count"],
+            "amount": datum["paymentInstruments"][0]["amount"],
+        }
+        for datum in data
+    ]
+
+
+def get_top_user_from_file(path):
+    data = json.load(open(path))["data"]
+    keys = ["states", "pincodes", "districts"]
+    to_return = []
+    for key in keys:
+        if data[key] is None:
+            continue
+        for item in data[key]:
+            to_return.append(
+                {
+                    "name": item["name"],
+                    "registered_users": item["registeredUsers"],
+                    "type": key,
+                }
+            )
+    return to_return
+
+
+def get_top_transaction_from_file(path):
+    data = json.load(open(path))["data"]
+    keys = ["states", "pincodes", "districts"]
+    to_return = []
+    for key in keys:
+        if data[key] is None:
+            continue
+        for item in data[key]:
+            to_return.append(
+                {
+                    "entity_name": item["entityName"],
+                    "count": item["metric"]["count"],
+                    "amount": item["metric"]["amount"],
+                    "type": item["metric"]["type"],
+                    "entity_type": key,
+                }
+            )
+    return to_return
+
+
+def extract_aggregated_user_rows(path):
+    start_path = path / Path("aggregated/user/country/india/")
+    all_data = []
+    # TODO: Facepalm, use bloody recursion
+    for directory in start_path.iterdir():
+        if is_state(directory):
+            for state in directory.iterdir():
+                for year_dir in state.iterdir():
+                    for data_file in year_dir.iterdir():
+                        year = int(year_dir.name)
+                        defaults = {
+                            "year": year,
+                            "aggregated_by": "state",
+                            "aggregate_name": state.name,
+                        }
+                        defaults.update(get_date_range(name=data_file.name, year=year))
+                        record = get_aggregated_user_from_file(data_file)
+                        record.update(defaults)
+                        all_data.append(record)
+        else:
+            year = int(directory.name)
+            for data_file in directory.iterdir():
+                defaults = {
+                    "year": year,
+                    "aggregated_by": "country",
+                    "aggregate_name": "india",
+                }
+                defaults.update(get_date_range(name=data_file.name, year=year))
+                record = get_aggregated_user_from_file(data_file)
+                record.update(defaults)
+                all_data.append(record)
+    return all_data
+
+
+def extract_aggregated_user_device_rows(path):
+    start_path = path / Path("aggregated/user/country/india/")
+    all_data = []
+    # TODO: Facepalm, use bloody recursion
+    for directory in start_path.iterdir():
+        if is_state(directory):
+            for state in directory.iterdir():
+                for year_dir in state.iterdir():
+                    for data_file in year_dir.iterdir():
+                        year = int(year_dir.name)
+                        defaults = {
+                            "year": year,
+                            "aggregated_by": "state",
+                            "aggregate_name": state.name,
+                        }
+                        defaults.update(get_date_range(name=data_file.name, year=year))
+                        records = get_aggregated_user_device_from_file(data_file)
+                        [record.update(defaults) for record in records]
+                        all_data.extend(records)
+        else:
+            year = int(directory.name)
+            for data_file in directory.iterdir():
+                defaults = {
+                    "year": year,
+                    "aggregated_by": "country",
+                    "aggregate_name": "india",
+                }
+                defaults.update(get_date_range(name=data_file.name, year=year))
+                records = get_aggregated_user_device_from_file(data_file)
+                [record.update(defaults) for record in records]
+                all_data.extend(records)
+    return all_data
+
+
+def extract_aggregated_transaction_rows(path):
+    start_path = path / Path("aggregated/transaction/country/india/")
+    all_data = []
+    # TODO: Facepalm, use bloody recursion
+    for directory in start_path.iterdir():
+        if is_state(directory):
+            for state in directory.iterdir():
+                for year_dir in state.iterdir():
+                    for data_file in year_dir.iterdir():
+                        year = int(year_dir.name)
+                        defaults = {
+                            "year": year,
+                            "aggregated_by": "state",
+                            "aggregate_name": state.name,
+                        }
+                        defaults.update(get_date_range(name=data_file.name, year=year))
+                        records = get_aggregated_transaction_from_file(data_file)
+                        [record.update(defaults) for record in records]
+                        all_data.extend(records)
+        else:
+            year = int(directory.name)
+            for data_file in directory.iterdir():
+                defaults = {
+                    "year": year,
+                    "aggregated_by": "country",
+                    "aggregate_name": "india",
+                }
+                defaults.update(get_date_range(name=data_file.name, year=year))
+                records = get_aggregated_transaction_from_file(data_file)
+                [record.update(defaults) for record in records]
+                all_data.extend(records)
+    return all_data
+
+
+def extract_top_user_rows(path):
+    start_path = path / Path("top/user/country/india/")
+    all_data = []
+    # TODO: Facepalm, use bloody recursion
+    for directory in start_path.iterdir():
+        if is_state(directory):
+            for state in directory.iterdir():
+                for year_dir in state.iterdir():
+                    for data_file in year_dir.iterdir():
+                        year = int(year_dir.name)
+                        defaults = {
+                            "year": year,
+                            "aggregated_by": "state",
+                            "aggregate_name": state.name,
+                        }
+                        defaults.update(get_date_range(name=data_file.name, year=year))
+                        records = get_top_user_from_file(data_file)
+                        [record.update(defaults) for record in records]
+                        all_data.extend(records)
+        else:
+            year = int(directory.name)
+            for data_file in directory.iterdir():
+                defaults = {
+                    "year": year,
+                    "aggregated_by": "country",
+                    "aggregate_name": "india",
+                }
+                defaults.update(get_date_range(name=data_file.name, year=year))
+                records = get_top_user_from_file(data_file)
+                [record.update(defaults) for record in records]
+                all_data.extend(records)
+    return all_data
+
+
+def extract_top_transaction_rows(path):
+    start_path = path / Path("top/transaction/country/india/")
+    all_data = []
+    # TODO: Facepalm, use bloody recursion
+    for directory in start_path.iterdir():
+        if is_state(directory):
+            for state in directory.iterdir():
+                for year_dir in state.iterdir():
+                    for data_file in year_dir.iterdir():
+                        year = int(year_dir.name)
+                        defaults = {
+                            "year": year,
+                            "aggregated_by": "state",
+                            "aggregate_name": state.name,
+                        }
+                        defaults.update(get_date_range(name=data_file.name, year=year))
+                        records = get_top_transaction_from_file(data_file)
+                        [record.update(defaults) for record in records]
+                        all_data.extend(records)
+        else:
+            year = int(directory.name)
+            for data_file in directory.iterdir():
+                defaults = {
+                    "year": year,
+                    "aggregated_by": "country",
+                    "aggregate_name": "india",
+                }
+                defaults.update(get_date_range(name=data_file.name, year=year))
+                records = get_top_transaction_from_file(data_file)
+                [record.update(defaults) for record in records]
+                all_data.extend(records)
+    return all_data
+
+
+def insert_data(directory: Path, db):
+    for name, all_data in [
+        ("aggregated_user_device", extract_aggregated_user_device_rows(directory)),
+        ("aggregated_user", extract_aggregated_user_rows(directory)),
+        ("aggregated_transaction", extract_aggregated_transaction_rows(directory)),
+        ("top_user", extract_top_user_rows(directory)),
+        ("top_transaction", extract_top_transaction_rows(directory)),
+    ]:
+        table = TABLE_NAMES[name]
+        db[table].insert_all(all_data)
+        print(f"Inserted {db[table].count} records in {table}")
+
+
+# Commands
+
+
+@click.command()
+@click.argument("directory", type=click.Path(exists=True))
+@click.option("--output", default="pulse.db", type=click.Path())
+def create_db(directory, output):
+    db_path = Path(output)
+
+    if db_path.exists():
+        error_console.print(
+            f"{output} location exists. Give a different location or delete the file"
+        )
+        exit(-1)
+
+    db = get_db(output)
+    create_tables(db)
+    data_dir = Path(directory)
+    insert_data(data_dir, db)
+    print(f"{output} created")
+
+
+if __name__ == "__main__":
+    create_db()
